4cacd13145b98f42f999f2a87f66296602b05d97
Akangkha <akangkhasarkar@gmail.com>
2025-03-12 03:04:21 +0530

    Added the foundation of the product microservice

---
+++ catalog/cmd/catalog/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"Goservices/catalog"
+	"log"
+	"time"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/tinrab/retry"
+)
+
+
+
+type Config struct{
+	 DatabaseURL string `envconfig:"DATABASE_URL"`
+}
+
+func main(){
+	var cf Config
+	err:=envconfig.Process("", &cf)
+    if err!=nil{
+		log.Fatal(err)
+	}
+
+	var r catalog.Repository
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		r, err = catalog.NewElasticRepository(cf.DatabaseURL)
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	})
+	defer r.Close()
+
+	log.Println("Listening on port 8080...")
+	s := catalog.NewService(r)
+	log.Fatal(catalog.ListenGRPC(s, "8000"))
+}
